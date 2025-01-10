e1a17c4e049c9a0cdfe023985ba2229c2074b528
noyzion <noyzion3@gmail.com>
2025-01-10 16:32:36 +0200

    added yarn

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,34 +1,29 @@
 import React from 'react';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './homeScreen'; // Import HomeScreen component
+import HomeScreen from '.'; // Import HomeScreen component
 import Login from './login'; // Import Login screen
 import { TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons'; // For home icon
-
-// Create navigators
-const Drawer = createDrawerNavigator();
-const Stack = createStackNavigator();
+import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
+import { MaterialIcons } from '@expo/vector-icons'; // For home icon
 
 // Create Stack for the Home screen
 const HomeStack = () => {
   return (
-    <Stack.Navigator initialRouteName="HomeScreen">
+    <Stack >
       <Stack.Screen
-        name="HomeScreen"
-        component={HomeScreen}
+        name="index"
         options={({ navigation }) => ({
           headerShown: true, // Show header with home icon
           headerLeft: () => (
             <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-              <Icon name="home" size={30} color="#000" style={{ marginLeft: 15 }} />
+              <MaterialIcons name="home" size={30} color="#000" style={{ marginLeft: 15 }} />
             </TouchableOpacity>
           ),
         })}
       />
-    </Stack.Navigator>
+    </Stack>
   );
 };
 
@@ -36,29 +31,20 @@ const App = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       {/* Drawer Navigator */}
-      <Drawer.Navigator
-        initialRouteName="Home" // The drawer will start with HomeStack
+      <Drawer
+        initialRouteName="login" // The drawer will start with HomeStack
         screenOptions={{
           drawerPosition: 'left', // Drawer on the left
         }}
       >
         <Drawer.Screen
-          name="Home"
-          component={HomeStack} // This gives access to HomeScreen through the StackNavigator
+          name="login"
           options={{
             title: 'Home', // Title in the header
             drawerLabel: 'Home', // This will show "Home" in the drawer
           }}
         />
-        <Drawer.Screen
-          name="Login"
-          component={Login}
-          options={{
-            drawerLabel: 'Login',
-            title: 'Login',
-          }}
-        />
-      </Drawer.Navigator>
+      </Drawer>
     </GestureHandlerRootView>
   );
 };
